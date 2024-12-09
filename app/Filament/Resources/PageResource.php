fe78743f576f7e1a675760339e965c1a86f916c9
Stan Daniëls <stan@teaminova.nl>
2024-12-09 14:43:14 +0100

    Show bug

---
+++ app/Filament/Resources/PageResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PageResource\Pages;
+use App\Filament\Resources\PageResource\RelationManagers;
+use App\Locale;
+use App\Models\Page;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PageResource extends Resource
+{
+    protected static ?string $model = Page::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\Select::make('foobar')
+                ->options([
+                    'Foo' => [
+                        10 => 'Option 10',
+                    ],
+                    'Bar' => [
+                        20 => 'Option 20',
+                    ],
+                ])
+                ->in(function (Select $component): array {
+                    dd($component->getEnabledOptions());
+                })
+                ->required(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPages::route('/'),
+            'create' => Pages\CreatePage::route('/create'),
+            'edit' => Pages\EditPage::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\PageResource\Pages;
+
+use App\Filament\Resources\PageResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePage extends CreateRecord
+{
+    protected static string $resource = PageResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        dd($data);
+        return parent::mutateFormDataBeforeCreate($data);
+    }
+}
